bbf745827c831277f78660687c73039e16daba25
jao321 <50146651+jao321@users.noreply.github.com>
2024-01-26 12:34:55 +0000

    Update main.py
    indentation_correct

--- YClon/main.py
+++ YClon/main.py
@@ -64,8 +64,8 @@ def clonotype(pre_clone, clonotypes, seqID, sequence_column, unico_pq_VJLen, tot
         clone = cluster_pre_clone.labels_
         maior = count + cluster_pre_clone.labels_.max() + 1
         junc_seq = junc_seq.reset_index(drop=True)
-	key = list(pre_clone["v_call"].unique())[0].split("*")[0]+","+list(pre_clone["j_call"].unique())[0].split("*")[0]+","+len(list(pre_clone[sequence_column].unique())[0])
-	clonotypes_df = pd.DataFrame(clonotypes[key])
+        key = list(pre_clone["v_call"].unique())[0].split("*")[0]+","+list(pre_clone["j_call"].unique())[0].split("*")[0]+","+len(list(pre_clone[sequence_column].unique())[0])
+        clonotypes_df = pd.DataFrame(clonotypes[key])
         clonotypes_df.columns = [seqID,sequence_column]
         tmp_df = pre_clone
         tmp_df.columns = ["tmp_seq",sequence_column]
@@ -86,7 +86,6 @@ def clonotype(pre_clone, clonotypes, seqID, sequence_column, unico_pq_VJLen, tot
         count = maior
         total_clust += cluster_pre_clone.labels_.max() + 1
         return unico_pq_VJLen, total_clust, maior
-
 
 
 def write_output(in_airr, seqID, out_filename, clonotipo, separator, seq_id_indx, vGene_indx, jGene_indx, junc_indx, short_output=False):
